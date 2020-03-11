65912fb0ef664105d56a0616d2def5d4ab9e0ff2
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-03-10 23:07:10 -0300

    Added animations

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    username : string;
 
+    constructor() {
+        this.username = '';
+    }
+
+    login() {
+        console.log(this.username);
+    }
 }
